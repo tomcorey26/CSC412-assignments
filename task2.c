1377734bb8344211c390702ec3dcf139beb6b05e
Tom Corey <tomcorey26@gmail.com>
2019-09-25 23:19:06 -0400

    task 2 done

--- task2.c
+++ task2.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-int isReapeat(char *str, char let) {
-  for (int i = 0; str[i] != '\0'; i++) {
+int isReapeat(char *str, char let)
+{
+  for (int i = 0; str[i] != '\0'; i++)
+  {
     if (let == str[i])
       return 1;
   }
   return 0;
 }
 
-char *sortByAsc(char *str) {
-  for (int i = 0; i < strlen(str); i++) {
-    for (int j = 0; j < strlen(str); j++) {
-      if (str[j] > str[i]) {
+char *sortByAsc(char *str)
+{
+  for (int i = 0; i < strlen(str); i++)
+  {
+    for (int j = 0; j < strlen(str); j++)
+    {
+      if (str[j] > str[i])
+      {
         char temp = str[i];
         str[i] = str[j];
         str[j] = temp;
@@ -23,31 +29,37 @@ char *sortByAsc(char *str) {
   return str;
 }
 
-void sortString(char *s) {
+void sortString(char *s)
+{
   // stores address of malloc allocated memory (first block of memory)
   // allocate memory for sorted string +1 for the null character
-  char *outStr = (char *)malloc(strlen(s) + 1);
   char *repeatStr = (char *)malloc(strlen(s) + 1);
 
-  // set last value to be null value
-  outStr[strlen(s)] = '\0';
-
+  // initialize count variables
   int letCount = 0;
   int numCount = 0;
   int othCount = 0;
   int currIdx = 0;
+
   // loop through input string and get counts
-  for (int i = 0; s[i] != '\0'; i++) {
-    if (!isReapeat(repeatStr, s[i])) {
+  for (int i = 0; s[i] != '\0'; i++)
+  {
+    if (!isReapeat(repeatStr, s[i]))
+    {
       // add to non repeat string
       repeatStr[currIdx] = s[i];
       currIdx++;
 
-      if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+      if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+      {
         letCount++;
-      } else if (s[i] <= '9' && s[i] >= '0') {
+      }
+      else if (s[i] <= '9' && s[i] >= '0')
+      {
         numCount++;
-      } else {
+      }
+      else
+      {
         othCount++;
       }
     }
@@ -56,17 +68,23 @@ void sortString(char *s) {
   char *numStr = (char *)malloc(numCount + 1);
   char *othStr = (char *)malloc(othCount + 1);
 
-  for (int i = 0; repeatStr[i] != '\0'; i++) {
+  for (int i = 0; repeatStr[i] != '\0'; i++)
+  {
     if ((repeatStr[i] >= 'a' && repeatStr[i] <= 'z') ||
-        (repeatStr[i] >= 'A' && repeatStr[i] <= 'Z')) {
+        (repeatStr[i] >= 'A' && repeatStr[i] <= 'Z'))
+    {
       // add to letter string
       letterStr[letCount - 1] = repeatStr[i];
       letCount--;
-    } else if (repeatStr[i] <= '9' && repeatStr[i] >= '0') {
+    }
+    else if (repeatStr[i] <= '9' && repeatStr[i] >= '0')
+    {
       // ad to num string
       numStr[numCount - 1] = repeatStr[i];
       numCount--;
-    } else {
+    }
+    else
+    {
       // add to oth string
       othStr[othCount - 1] = repeatStr[i];
       othCount--;
@@ -80,13 +98,16 @@ void sortString(char *s) {
   printf("\"%s", letterStr);
   printf("%s", numStr);
   printf("%s\"\n", othStr);
+
   return;
 }
 
-int main() {
+int main()
+{
   char in[100];
   // printf() displays the string inside quotation
-  while (strcmp(in, "EXIT") != 0) {
+  while (strcmp(in, "EXIT") != 0)
+  {
     printf("enter an input string:");
     scanf(" %[^\n]", in);
     sortString(in);
