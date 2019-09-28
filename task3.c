9b5892b570bdd08a3f9ee60cc6e2ad1127eec383
Tom Corey <tomcorey26@gmail.com>
2019-09-28 15:08:11 -0400

    task 3 finished

--- task3.c
+++ task3.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct Category
+{
+  int index;
+  char name[50];
+  char ranges[20][2];
+};
+struct Categorys
+{
+  int categoryAmount;
+  struct Category categorys[];
+};
+
 int isReapeat(char *str, char let)
 {
   for (int i = 0; str[i] != '\0'; i++)
@@ -29,8 +41,29 @@ char *sortByAsc(char *str)
   return str;
 }
 
+int isInRange(char currStr, char *range)
+{
+  if (currStr >= range[0] && currStr <= range[1])
+  {
+    return 1;
+  }
+  return 0;
+}
+
 void sortString(char *s)
 {
+  struct Category letter =
+      {
+          1,
+          "letter",
+          {{'a', 'z'}, {'A', 'Z'}}};
+
+  struct Category number =
+      {
+          2,
+          "number",
+          {{'0', '9'}}};
+
   // stores address of malloc allocated memory (first block of memory)
   // allocate memory for sorted string +1 for the null character
   char *repeatStr = (char *)malloc(strlen(s) + 1);
@@ -50,11 +83,12 @@ void sortString(char *s)
       repeatStr[currIdx] = s[i];
       currIdx++;
 
-      if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+      if ((isInRange(s[i], letter.ranges[0])) ||
+          (isInRange(s[i], letter.ranges[1])))
       {
         letCount++;
       }
-      else if (s[i] <= '9' && s[i] >= '0')
+      else if (isInRange(s[i], number.ranges[0]))
       {
         numCount++;
       }
@@ -70,14 +104,14 @@ void sortString(char *s)
 
   for (int i = 0; repeatStr[i] != '\0'; i++)
   {
-    if ((repeatStr[i] >= 'a' && repeatStr[i] <= 'z') ||
-        (repeatStr[i] >= 'A' && repeatStr[i] <= 'Z'))
+    if (isInRange(repeatStr[i], letter.ranges[0]) ||
+        isInRange(repeatStr[i], letter.ranges[1]))
     {
       // add to letter string
       letterStr[letCount - 1] = repeatStr[i];
       letCount--;
     }
-    else if (repeatStr[i] <= '9' && repeatStr[i] >= '0')
+    else if (isInRange(repeatStr[i], number.ranges[0]))
     {
       // ad to num string
       numStr[numCount - 1] = repeatStr[i];
