6100fe312fe657fe2d0614e8e968349d67403262
tomcorey26 <tomcorey26@gmail.com>
2019-11-02 11:26:05 -0400

    tile.c working

---
+++ prog04/Prog 03 Solution/Prog 03/tile.c
@@ -0,0 +1,68 @@
+//
+// Created by Tom Corey on 11/1/19.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+//
+#include "../Image_IO/imageIO_TGA.h"
+#include "../Image_IO/RasterImage.h"
+
+//--------------------------------------------------------------
+//	Main function, expecting as arguments:
+//		<images> outFolderPath
+//	It returns an error code (0 for no error)
+//--------------------------------------------------------------
+struct Grid {
+    int rows;
+    int cols;
+};
+
+// Write tile to specified spot on grid
+void writeTile(RasterImage* big, RasterImage* tile, int row, int col) {
+    int** b2D = (int **) big->raster2D;
+    int** t2D = (int **) tile->raster2D;
+
+    int ib;
+    int jb;
+    for (int it = 0,ib=row * tile->numRows; it < tile->numRows; it++,ib++) {
+        for (int jt = 0,jb=col * tile->numCols; jt < tile->numCols; jt++,jb++) {
+            b2D[ib][jb] = t2D[it][jt];
+        }
+    }
+
+
+}
+
+int main(int argc, char *argv[])
+{
+    char *outFolderPath = argv[argc - 1];
+
+    //get width and height of sprite tile
+    //square root closest to number of tiles
+    int tileCount = argc-2;
+    double gNumRows = ceil(sqrt(tileCount));
+    double gNumCols = ceil((float)tileCount/gNumRows);
+
+    struct Grid spriteSheet = {gNumRows,gNumCols};
+    RasterImage currTile = readTGA(argv[1]);
+    RasterImage spriteGrid = newRasterImage(spriteSheet.rows * currTile.numRows ,spriteSheet.cols * currTile.numCols,RGBA32_RASTER);
+
+    int currTileIdx = 1;
+    for (int i = spriteSheet.rows - 1 ; i >= 0 ; i --){
+        for (int j = 0; j < spriteSheet.cols; j++) {
+                if (currTileIdx < argc -1) {
+                    currTile = readTGA(argv[currTileIdx]);
+                    writeTile(&spriteGrid,&currTile,i,j);
+                    currTileIdx++;
+                    printf("%d\n", currTileIdx);
+                }
+        }
+    }
+
+    writeTGA(outFolderPath, &spriteGrid);
+    deleteRasterImage(&currTile);
+    deleteRasterImage(&spriteGrid);
+}
