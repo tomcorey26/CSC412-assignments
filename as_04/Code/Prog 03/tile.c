aff238fbcd155f3027e36d22582d7ac5178fc621
Tom Corey <tomcorey26@gmail.com>
2019-11-02 13:53:58 -0400

    pictures

--- as_04/Code/Prog 03/tile.c
+++ as_04/Code/Prog 03/tile.c
@@ -15,25 +15,34 @@
 //		<images> outFolderPath
 //	It returns an error code (0 for no error)
 //--------------------------------------------------------------
-struct Grid {
+struct Grid
+{
     int rows;
     int cols;
 };
 
-// Write tile to specified spot on grid
-void writeTile(RasterImage* big, RasterImage* tile, int row, int col) {
-    int** b2D = (int **) big->raster2D;
-    int** t2D = (int **) tile->raster2D;
+/**
+ * Writes tile to specified row and coloum of tile grid
+ * @param big raster of the big grid 
+ * @param tile raster of the tile
+ * @param row specified row for grid
+ * @param col specified col for grid
+ * 
+**/
+void writeTile(RasterImage *big, RasterImage *tile, int row, int col)
+{
+    int **b2D = (int **)big->raster2D;
+    int **t2D = (int **)tile->raster2D;
 
     int ib;
     int jb;
-    for (int it = 0,ib=row * tile->numRows; it < tile->numRows; it++,ib++) {
-        for (int jt = 0,jb=col * tile->numCols; jt < tile->numCols; jt++,jb++) {
+    for (int it = 0, ib = row * tile->numRows; it < tile->numRows; it++, ib++)
+    {
+        for (int jt = 0, jb = col * tile->numCols; jt < tile->numCols; jt++, jb++)
+        {
             b2D[ib][jb] = t2D[it][jt];
         }
     }
-
-
 }
 
 int main(int argc, char *argv[])
@@ -42,23 +51,26 @@ int main(int argc, char *argv[])
 
     //get width and height of sprite tile
     //square root closest to number of tiles
-    int tileCount = argc-2;
+    int tileCount = argc - 2;
     double gNumRows = ceil(sqrt(tileCount));
-    double gNumCols = ceil((float)tileCount/gNumRows);
+    double gNumCols = ceil((float)tileCount / gNumRows);
 
-    struct Grid spriteSheet = {gNumRows,gNumCols};
+    struct Grid spriteSheet = {gNumRows, gNumCols};
     RasterImage currTile = readTGA(argv[1]);
-    RasterImage spriteGrid = newRasterImage(spriteSheet.rows * currTile.numRows ,spriteSheet.cols * currTile.numCols,RGBA32_RASTER);
+    RasterImage spriteGrid = newRasterImage(spriteSheet.rows * currTile.numRows, spriteSheet.cols * currTile.numCols, RGBA32_RASTER);
 
     int currTileIdx = 1;
-    for (int i = spriteSheet.rows - 1 ; i >= 0 ; i --){
-        for (int j = 0; j < spriteSheet.cols; j++) {
-                if (currTileIdx < argc -1) {
-                    currTile = readTGA(argv[currTileIdx]);
-                    writeTile(&spriteGrid,&currTile,i,j);
-                    currTileIdx++;
-                    printf("%d\n", currTileIdx);
-                }
+    for (int i = spriteSheet.rows - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < spriteSheet.cols; j++)
+        {
+            if (currTileIdx < argc - 1)
+            {
+                currTile = readTGA(argv[currTileIdx]);
+                writeTile(&spriteGrid, &currTile, i, j);
+                currTileIdx++;
+                printf("%d\n", currTileIdx);
+            }
         }
     }
 
